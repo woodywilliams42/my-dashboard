db98b20d69692fc935fae91d3449b02ccfbba13f
woodywilliams42 <woody@itwilliams.com>
2025-06-05 22:47:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  <!-- === SCRIPT === -->
+
 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
   import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
