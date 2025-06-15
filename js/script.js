7a239b248fe87cb51aa6988ffc46d388208aa1e2
woodywilliams42 <woody@itwilliams.com>
2025-06-15 10:08:55 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-  import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-  import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAFhr8C3o6fEXY1vNkiKq_0tfXp45ekTlU",
-    authDomain: "woodydashboard.firebaseapp.com",
-    projectId: "woodydashboard",
-    storageBucket: "woodydashboard.appspot.com",
-    messagingSenderId: "180582118415",
-    appId: "1:180582118415:web:447bdf09b42dd16fa15f7a"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  const storage = getStorage(app);
-
-  window.db = db;
-  window.storage = storage;
-
-
+import { db, storage } from './firebase.js';
 
 async function saveNotes(tab) {
   const content = document.getElementById(`notes-${tab}`).value;
