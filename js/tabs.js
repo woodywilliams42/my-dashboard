0712e304959b73bc3550b5494298e64b29865621
woodywilliams42 <woody@itwilliams.com>
2025-06-17 00:40:47 +0100

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -60,6 +60,7 @@ function setupTabSwitching() {
       if (clockWrapper) {
         clockWrapper.style.display = target === "work" ? "block" : "none";
       }
+ loadFramesForTab(tabId);
     });
   });
 }
