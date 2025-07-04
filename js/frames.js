9b50d8eba66e5bb26728405fc182c4c4d562f0d4
woodywilliams42 <woody@itwilliams.com>
2025-07-04 15:25:18 +0100

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -1,4 +1,4 @@
-import { db } from './firebase.js';
+import { db } from './firebase.js'; 
 import { doc, getDoc, setDoc } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 import { setupBookmarkFrame } from './bookmark.js';
 import { setupTimerFrame } from './timer.js';
