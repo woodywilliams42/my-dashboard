910e83ac2b707c79ddbd7831b8e6ef76b281f01d
woodywilliams42 <woody@itwilliams.com>
2025-07-15 20:59:39 +0100

    Update alarms.js

--- js/alarms.js
+++ js/alarms.js
@@ -1,10 +1,7 @@
-// alarms.js
-import { db } from './firebase.js';
-import {
-  doc,
-  getDoc,
-  setDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { app } from './firebase.js';
+
+const db = getFirestore(app);
 
 // --- Settings and Sounds ---
 const alarmSettings = {
