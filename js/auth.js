8e0a640bb2e06a1c54fad4c0efdd31f4742d7cf7
woodywilliams42 <woody@itwilliams.com>
2025-07-24 00:05:14 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -18,7 +18,7 @@ authBtn.classList.add("logged-out");
 authBtn.title = "Sign in to Google";
 
 const img = document.createElement("img");
-img.src = "images/google-icon.png";
+img.src = "images/g-logo.png";
 img.alt = "Google Sign-In";
 authBtn.appendChild(img);
 
